bc75bb64d48b45e47ce9beafb516aa0fcbff9ec4
AAK581 <adhamahmad@outlook.com>
2023-04-25 16:51:45 -0700

    fix

--- main.h
+++ main.h
@@ -99,8 +99,8 @@ buffer_t *init_buffer(void);
 void free_buffer(buffer_t *output);
 unsigned int _memcpy(buffer_t *output, const char *src,
 		unsigned int n);
-unsigned int convert_sbase(buffer_t *output, const char *src,
-		unsigned int n);
+unsigned int convert_sbase(buffer_t *output, long int num, char *base,
+		unsigned char flags, int wid, int prec);
 unsigned int convert_ubase(buffer_t *output, unsigned long int num,
 		char *base, unsigned char flags, int wid, int prec);
 
